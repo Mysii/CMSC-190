ef87d5f71ce466910a58a1726b4be1d14363c53c
Azha Vianca de Belen <azhaviancadebelen@gmail.com>
2017-05-16 14:45:54 +0800

    new branch heroku

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -17,7 +17,7 @@ class TransactionsController extends Controller
 
      public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('auth');
     }
     
     /**
