3cb3cfedf0c8a44a08f7d89a59cb1395607d25ea
Azha Vianca de Belen <azhaviancadebelen@gmail.com>
2017-05-16 12:54:00 +0800

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
+
 <?php
-$url = parse_url(getenv("DATABASE_URL"));
 
 return [
 
@@ -55,14 +55,16 @@ return [
         ],
 
         'pgsql' => [
-    'driver'   => 'pgsql',
-    'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-    'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-    'username' => parse_url(getenv("DATABASE_URL"))["user"],
-    'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-    'charset'  => 'utf8',
-    'prefix'   => '',
-    'schema'   => 'public',
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'sims'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'starazha?'),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
 ],
 
     ],
