a37bb15a9ff0be6c49c408b4c80ccae5e04232c3
Azha Vianca de Belen <azhaviancadebelen@gmail.com>
2017-05-16 12:37:32 +0800

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -1,11 +1,6 @@
 <?php
 $url = parse_url(getenv("DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -59,16 +54,16 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => array(
-        'driver'   => 'pgsql',
-        'host'     => $host,
-        'database' => $database,
-        'username' => $username,
-        'password' => $password,
-        'charset'  => 'utf8',
-        'prefix'   => '',
-        'schema'   => 'public',
-    ),
+        'pgsql' => [
+    'driver'   => 'pgsql',
+    'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+    'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+    'username' => parse_url(getenv("DATABASE_URL"))["user"],
+    'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+    'charset'  => 'utf8',
+    'prefix'   => '',
+    'schema'   => 'public',
+],
 
     ],
 
